aff2a9fac8b5f7f59981c2f66d5a3c7f7b197a20
lastohkad <lastochka.d@mail.ru>
2024-09-24 18:27:53 +0700

    Задание 9

---
+++ Lesson_9/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+# from Lesson8.constants import X_client_URl
+
+# @pytest.fixture()
+# def get_token(username='bloom', password='fire-fairy'):
+#     login_pass = {'username': username, 'password': password}
+#     resp_token = requests.post(X_client_URl + '/auth/login', json=login_pass)
+#     token = resp_token.json()['userToken']
+#     return token
+
+url = "https://x-clients-be.onrender.com"
